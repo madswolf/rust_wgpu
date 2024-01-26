6b8620aa03cd7e4a78b7d984caf857af6f613161
MadsWJespersen <Mads_1997@live.dk>
2024-01-26 01:06:27 +0100

    Added color shader program and switching between them (need debouncing or something for keyboard input)

---
+++ src/color_shader.wgsl
@@ -0,0 +1,24 @@
+// Vertex shader
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) vert_pos: vec2<f32>,
+};
+
+@vertex
+fn vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    var out: VertexOutput;
+    let x = f32(1 - i32(in_vertex_index)) * 0.5;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+    out.vert_pos = vec2<f32>(x,y);
+    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    return out;
+}
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    
+    return vec4<f32>(in.vert_pos, 0.5, 1.0);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@ struct State {
     // unsafe references to the window's resources.
     window: Window,
     render_pipeline: wgpu::RenderPipeline,
+    color_render_pipeline: wgpu::RenderPipeline,
+    use_color: bool,
 }
 
 
@@ -99,37 +101,71 @@ impl State {
             layout: Some(&render_pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
-                entry_point: "vs_main", // 1.
-                buffers: &[], // 2.
+                entry_point: "vs_main",
+                buffers: &[],
             },
-            fragment: Some(wgpu::FragmentState { // 3.
+            fragment: Some(wgpu::FragmentState {
                 module: &shader,
                 entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState { // 4.
+                targets: &[Some(wgpu::ColorTargetState {
                     format: config.format,
                     blend: Some(wgpu::BlendState::REPLACE),
                     write_mask: wgpu::ColorWrites::ALL,
                 })],
             }),
             primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw, // 2.
+                front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
                 polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
                 unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
                 conservative: false,
             },
-            depth_stencil: None, // 1.
+            depth_stencil: None,
             multisample: wgpu::MultisampleState {
-                count: 1, // 2.
-                mask: !0, // 3.
-                alpha_to_coverage_enabled: false, // 4.
+                count: 1, 
+                mask: !0, 
+                alpha_to_coverage_enabled: false,
             },
-            multiview: None, // 5.
+            multiview: None,
+        });
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("color_shader.wgsl"));
+
+        let color_render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1, 
+                mask: !0, 
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
         });        
 
         surface.configure(&device, &config);
@@ -144,6 +180,8 @@ impl State {
             clear_color,
             size,
             render_pipeline,
+            color_render_pipeline,
+            use_color:false,
         }
     }
 
@@ -170,6 +208,15 @@ impl State {
                     a: 1.0,
                 };
                 true
+            }
+            WindowEvent::KeyboardInput { input, ..} => {
+                match input.virtual_keycode {
+                    Some(VirtualKeyCode::C) => {
+                        self.use_color = !self.use_color;
+                        true
+                    },
+                    _ => false
+                }
             }
             _ => false,
         }
@@ -210,7 +257,12 @@ impl State {
                 timestamp_writes: None,
             });
         
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
+            if self.use_color {
+                render_pass.set_pipeline(&self.color_render_pipeline);
+            } else {
+                render_pass.set_pipeline(&self.render_pipeline); // 2.
+            }
+
             render_pass.draw(0..3, 0..1); // 3.
     
         }
@@ -268,7 +320,6 @@ pub async fn run() {
                 window_id,
             } if window_id == state.window().id() => {
                 if !state.input(event) {
-                    // UPDATED!
                     match event {
                         WindowEvent::CloseRequested
                         | WindowEvent::KeyboardInput {
